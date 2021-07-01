94e47fd87c28fd76605f9b25df33b4525e69e9ce
Fikret <fburgazli@hotmail.com>
2021-06-30 21:16:37 -0400

    started us-102 features added

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.api.FeatureConstructor;
+import javafx.beans.binding.When;
 import org.junit.Test;
 
 public class UserStory102 {
@@ -5,5 +7,9 @@ public class UserStory102 {
     public void test(){
         System.out.println("US-102 test case:");
     }
+//    @FeatureConstructor
+//    Given User is on login page
+//    When User click on Home page
+//    Then User should land on Home page
 
 }
