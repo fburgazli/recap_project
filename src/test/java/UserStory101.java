78567a68c2435f745309bb40e655a17d55d6ff92
Fikret <fburgazli@hotmail.com>
2021-06-30 20:20:51 -0400

    starter us-101 push to remote first time

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -1,5 +1,6 @@
 public class UserStory101 {
     public static void main(String[] args) {
         System.out.println("pushing local branch to remote");
+        System.out.println("this is first time we push branch to remote");
     }
 }
