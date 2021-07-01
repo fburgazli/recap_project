43cf8d5d94f39cd5fe1a3e4ef190a161f14d7511
Fikret <fburgazli@hotmail.com>
2021-06-30 20:25:17 -0400

    starter us-101 todo added

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -2,5 +2,6 @@ public class UserStory101 {
     public static void main(String[] args) {
         System.out.println("pushing local branch to remote");
         System.out.println("this is first time we push branch to remote");
+        //todo : continue later on this works
     }
 }
